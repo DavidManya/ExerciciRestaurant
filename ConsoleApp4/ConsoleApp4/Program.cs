62844daa521b99652b7996e03715ee8dafbf15da
David Manyà <david.manya@gmail.com>
2020-01-17 17:14:34 +0100

    Pujada FASE 1

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fase 1
+            int bitllet5 = 5;
+            int bitllet10 = 10;
+            int bitllet20 = 20;
+            int bitllet50 = 50;
+            int bitllet100 = 100;
+            int bitllet200 = 200;
+            int bitllet500 = 500;
+
+            string plat1 = "";
+            string plat2 = "";
+            string plat3 = "";
+            string plat4 = "";
+            string plat5 = "";
+
+            double preu1 = 0;
+            double preu2 = 0;
+            double preu3 = 0;
+            double preu4 = 0;
+            double preu5 = 0;
+
+            string[] ArrayMenu = {plat1, plat2, plat3, plat4, plat5};
+            double[] ArrayPreu = {preu1, preu2, preu3, preu4, preu5};
+        }
+    }
+}
